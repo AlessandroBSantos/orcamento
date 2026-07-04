760c5e38edf883ad336a4ba644e35bffe39634a0
AlessandroBSantos <alessandrobsantos1012@gmail.com>
2026-07-04 19:03:24 -0300

    ajuste na conexao com o banco

--- config/database.php
+++ config/database.php
@@ -44,26 +44,3 @@ class Database
         return self::$connection;
     }
 }
-
-$host = "localhost";
-$banco = "ales7542_lla_erp";
-$usuario = "ales7542_lla_erp_user";
-$senha = "&KCiM?1sb0HI";
-
-try {
-
-    $pdo = new PDO(
-        "mysql:host=$host;dbname=$banco;charset=utf8mb4",
-        $usuario,
-        $senha,
-        [
-            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
-        ]
-    );
-
-} catch (PDOException $e) {
-
-    die("Erro na conexão: " . $e->getMessage());
-
-}
