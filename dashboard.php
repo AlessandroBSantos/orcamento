82591d31e2e464007a8d89adf26f38687a5a9d04
AlessandroBSantos <alessandrobsantos1012@gmail.com>
2026-07-04 12:57:26 -0300

    teste de erro

--- dashboard.php
+++ dashboard.php
@@ -1,5 +1,15 @@
 <?php
 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+require_once 'includes/auth.php';
+require_once 'queries/dashboard.php';
+
+
+
+
 require_once 'includes/auth.php';
 require_once 'queries/dashboard.php';
 
