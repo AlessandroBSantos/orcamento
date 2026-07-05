b1ba79a4f4bb9b79cbf20dc029c8d84e3ef6c1f0
Alessandro Batista dos Santos <alessandrobsantos1012@gmail.com>
2026-07-05 16:11:10 -0300

    refactor: reorganize index.php for improved readability and structure

--- pages/clientes/index.php
+++ pages/clientes/index.php
@@ -1,119 +1,31 @@
 <?php
 
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+echo "1 - Inicio<br>";
+
 $titulo = "Clientes";
 
-echo "<h1 style='color:red'>INDEX CARREGADO</h1>"; 
-
+echo "2 - Titulo OK<br>";
 
 require_once '../../includes/layout_inicio.php';
+
+echo "3 - Layout carregado<br>";
+
 require_once '../../controllers/ClienteController.php';
+
+echo "4 - Controller carregado<br>";
 
 $controller = new ClienteController();
 
+echo "5 - Controller instanciado<br>";
+
 $clientes = $controller->index();
 
-?>
+echo "6 - Consulta realizada<br>";
 
-<div class="dashboard-header">
-
-    <h1>Clientes</h1>
-
-    <a href="novo.php" class="btn">
-        + Novo Cliente
-    </a>
-
-</div>
-
-<?php if (isset($_GET['sucesso'])): ?>
-
-<div class="alert alert-success">
-
-    Cliente cadastrado com sucesso!
-
-</div>
-
-<?php endif; ?>
-
-<div class="panel">
-
-    <table>
-
-<thead>
-
-<tr>
-
-    <th>ID</th>
-
-    <th>Nome</th>
-
-    <th>Cidade</th>
-
-    <th>CPF/CNPJ</th>
-
-    <th>Status</th>
-
-    <th>Ações</th>
-
-</tr>
-
-</thead>
-<tbody>
-
-<?php if(!empty($clientes)): ?>
-
-<?php foreach($clientes as $cliente): ?>
-
-<tr>
-
-    <td><?= $cliente['id'] ?></td>
-
-    <td><?= htmlspecialchars($cliente['nome']) ?></td>
-
-    <td><?= htmlspecialchars($cliente['cidade']) ?></td>
-
-    <td><?= htmlspecialchars($cliente['cpf_cnpj']) ?></td>
-
-    <td><?= htmlspecialchars($cliente['status']) ?></td>
-
-    <td>
-
-        <a href="editar.php?id=<?= $cliente['id'] ?>">
-
-            ✏ Editar
-
-        </a>
-
-    </td>
-
-</tr>
-
-<?php endforeach; ?>
-
-<?php else: ?>
-
-<tr>
-
-    <td colspan="6">
-
-        Nenhum cliente encontrado.
-
-    </td>
-
-</tr>
-
-<?php endif; ?>
-
-</tbody>
-        
-
-
-
-    </table>
-
-</div>
-
-<?php
-
-require_once '../../includes/layout_fim.php';
-
+echo "<pre>";
+print_r($clientes);
+echo "</pre>";
 ?>
