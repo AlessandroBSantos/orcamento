ed44b6efeb72a681ddb705bb5abd77fa41370a47
Alessandro Batista dos Santos <alessandrobsantos1012@gmail.com>
2026-07-04 22:19:16 -0300

    feat: add novo.php for client registration page

---
+++ pages/clientes/novo.php
@@ -0,0 +1,30 @@
+<?php
+
+$titulo = "Novo Cliente";
+
+require_once '../../includes/layout_inicio.php';
+
+?>
+
+<div class="dashboard-header">
+
+    <h1>Novo Cliente</h1>
+
+    <a href="index.php" class="btn">
+
+        Voltar
+
+    </a>
+
+    <div class="panel">
+        <h2>Casdastro de Cliente</h2>
+
+        <p>Esta é a apagina de cadastro de clientes.</p>
+
+        </div>
+
+    <?php
+
+    require_once '../../includes/layout_fim.php';
+
+    ?>
