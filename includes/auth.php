d00fb668fd08cd3cdacf6d06205213d499e1decb
Alessandro Batista dos Santos <alessandrobsantos1012@gmail.com>
2026-07-05 21:07:40 -0300

    refactor: improve structure and readability of index.php, enhance client listing display

--- includes/auth.php
+++ includes/auth.php
@@ -1,9 +1,12 @@
 <?php
-session_start();
+
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
 
 if (!isset($_SESSION['usuario_id'])) {
 
     header("Location: /orcamento/login.php");
     exit;
+
 }
-?>
