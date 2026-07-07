3164fed609d79216f38f8ba409cf3c2ee13ace28
AlessandroBSantos <153738292+AlessandroBSantos@users.noreply.github.com>
2026-07-07 14:08:59 -0300

    formulario editar cliente

--- pages/clientes/editar.php
+++ pages/clientes/editar.php
@@ -43,17 +43,122 @@ if (!$cliente) {
 
 </div>
 
+<form action="atualizar.php" method="POST">
+
+<input
+    type="hidden"
+    name="id"
+    value="<?= $cliente['id'] ?>">
+
 <div class="panel">
 
-    <h2>Dados do Cliente</h2>
+<h2>Dados Gerais</h2>
 
-    <pre>
+<div class="form-grid">
 
-<?php print_r($cliente); ?>
+<div class="form-group">
 
-    </pre>
+<label>Tipo</label>
+
+<select name="tipo">
+
+<option value="PF" <?= $cliente['tipo']=="PF"?"selected":"" ?>>
+
+Pessoa Física
+
+</option>
+
+<option value="PJ" <?= $cliente['tipo']=="PJ"?"selected":"" ?>>
+
+Pessoa Jurídica
+
+</option>
+
+</select>
 
 </div>
+
+<div class="form-group">
+
+<label>Status</label>
+
+<select name="status">
+
+<option value="Ativo" <?= $cliente['status']=="Ativo"?"selected":"" ?>>
+
+Ativo
+
+</option>
+
+<option value="Inativo" <?= $cliente['status']=="Inativo"?"selected":"" ?>>
+
+Inativo
+
+</option>
+
+</select>
+
+</div>
+
+<div class="form-group">
+
+<label>Nome</label>
+
+<input
+type="text"
+name="nome"
+value="<?= htmlspecialchars($cliente['nome']) ?>">
+
+</div>
+
+<div class="form-group">
+
+<label>Nome Fantasia</label>
+
+<input
+type="text"
+name="nome_fantasia"
+value="<?= htmlspecialchars($cliente['nome_fantasia']) ?>">
+
+</div>
+
+<div class="form-group">
+
+<label>CPF/CNPJ</label>
+
+<input
+type="text"
+name="cpf_cnpj"
+value="<?= htmlspecialchars($cliente['cpf_cnpj']) ?>">
+
+</div>
+
+<div class="form-group">
+
+<label>RG / IE</label>
+
+<input
+type="text"
+name="rg_ie"
+value="<?= htmlspecialchars($cliente['rg_ie']) ?>">
+
+</div>
+
+</div>
+
+</div>
+
+<br>
+
+<button
+type="submit"
+class="btn">
+
+💾 Salvar Alterações
+
+</button>
+
+</form>
 
 <?php
 
