1c4d8072212358f526d9cde5d40db8303ecfa6a3
AlessandroBSantos <153738292+AlessandroBSantos@users.noreply.github.com>
2026-07-08 13:40:14 -0300

    delete de arquivos, criacao de produtos

---
+++ pages/produtos/novo.php
@@ -0,0 +1,27 @@
+<?php
+
+$titulo = "Novo Produto";
+
+require_once '../../includes/layout_inicio.php';
+
+?>
+
+<div class="dashboard-header">
+
+    <div>
+
+        <h1>Novo Produto</h1>
+
+        <p>Cadastro de Produtos</p>
+
+    </div>
+
+    <a href="index.php" class="btn btn-primary">
+
+        ← Voltar
+
+    </a>
+
+</div>
+
+<form action="salvar.php" method="POST">
