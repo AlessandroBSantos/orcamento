58b6276bef27fa9077d4692a18639326b3ac858b
AlessandroBSantos <153738292+AlessandroBSantos@users.noreply.github.com>
2026-07-08 16:45:55 -0300

    ajuste

--- pages/produtos/index.php
+++ pages/produtos/index.php
@@ -11,59 +11,6 @@ $clientes = $controller->index();
 
 ?>
 
-<div class="dashboard-header">
-
-    <div>
-
-        <h1>Clientes</h1>
-
-        <p>Cadastro de Clientes</p>
-
-    </div>
-
-    <a href="novo.php" class="btn">
-
-        + Novo Cliente
-
-    </a>
-
-</div>
-
-<?php if(isset($_GET['sucesso'])): ?>
-
-<div class="alert alert-success">
-
-<?php
-
-switch($_GET['sucesso']){
-
-    case "cadastrado":
-
-        echo "Cliente cadastrado com sucesso.";
-
-        break;
-
-    case "editado":
-
-        echo "Cliente atualizado com sucesso.";
-
-        break;
-
-    case "excluido":
-
-        echo "Cliente excluído com sucesso.";
-
-        break;
-
-}
-
-?>
-
-</div>
-
-<?php endif; ?>
-
-
 
 <?php
 
