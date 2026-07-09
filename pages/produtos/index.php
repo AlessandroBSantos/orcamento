864dd134383d202068c54b56b8c476fcfad86a3b
AlessandroBSantos <153738292+AlessandroBSantos@users.noreply.github.com>
2026-07-09 10:10:25 -0300

    ajuste no index.phph produto

--- pages/produtos/index.php
+++ pages/produtos/index.php
@@ -1,19 +1,29 @@
 <?php
 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 $titulo = "Produtos";
 
 require_once '../../includes/layout_inicio.php';
 
+echo "<h2>Passo 1</h2>";
+
 require_once '../../controllers/ProdutoController.php';
 
-echo "Index 1<br>";
+echo "<h2>Passo 2</h2>";
 
 $controller = new ProdutoController();
 
-echo "Index 2<br>";
+echo "<h2>Passo 3</h2>";
 
 $produtos = $controller->index();
 
-echo "Index 3<br>";
+echo "<h2>Passo 4</h2>";
+
+echo "<pre>";
+print_r($produtos);
+echo "</pre>";
 
 require_once '../../includes/layout_fim.php';
