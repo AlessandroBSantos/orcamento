7ac82a76441a332257812ea314ef494c6af19059
AlessandroBSantos <153738292+AlessandroBSantos@users.noreply.github.com>
2026-07-10 13:24:54 -0300

    ajuste novo

--- pages/produtos/novo.php
+++ pages/produtos/novo.php
@@ -2,6 +2,18 @@
 
 $titulo = "Novo Produto";
 
+/*
+|--------------------------------------------------------------------------
+| JavaScripts desta página
+|--------------------------------------------------------------------------
+*/
+
+$scripts = [
+    'tabs.js',
+    'produtos.js'
+];
+
+
 require_once '../../includes/layout_inicio.php';
 
 require_once '../../controllers/ProdutoController.php';
