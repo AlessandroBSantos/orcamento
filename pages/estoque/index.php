6250d479ab367c9a31494dc8c898629bed3daa49
AlessandroBSantos <153738292+AlessandroBSantos@users.noreply.github.com>
2026-07-18 16:37:05 -0300

    init set erros

--- pages/estoque/index.php
+++ pages/estoque/index.php
@@ -1,4 +1,10 @@
-<?php 
+<?php
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+
 $titulo = "Estoque";
 require_once '../../controllers/EstoqueController.php';
 $controller = new EstoqueController();
